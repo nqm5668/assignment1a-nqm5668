6fec7dc1b806ae7a4b92e0598d741e8d9e349962
Nur Ariana Syazmeen Mohamad Taufik <nqm5668@psu.edu>
2024-09-13 21:52:49 -0400

    Updated write_ids.c with GitHub username and PSU student ID

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "nqm5668";
+    char STUDENT_ID[] = "983991908";
 
     return 0 ;
 }
